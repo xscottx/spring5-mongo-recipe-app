1d64611c1b3972a1fbc6bbc2412d7febe07bf94d
Hoang Vo <hoangvo@gmail.com>
2018-12-19 15:36:31 -0600

    fix: integration tests

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,7 +2,6 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
